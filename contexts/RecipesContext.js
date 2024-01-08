7a25b3b80e2f14ccf1059f9597b133216c6a5953
912-Danciu-Diana <diana.ioana.danciu@gmail.com>
2024-01-08 13:55:06 +0200

    first commit

--- contexts/RecipesContext.js
+++ contexts/RecipesContext.js
@@ -1,84 +1,42 @@
 import React, { createContext, useState, useEffect } from 'react';
 import databaseManager from '../database/DatabaseManager';
-import * as Network from 'expo-network';
-import {
-    fetchRecipesFromServer,
-    addRecipeToServer,
-    updateRecipeOnServer,
-    deleteRecipeFromServer,
-    deleteAllRecipesFromServer,
-    checkServerAvailability
-} from '../services/apiService';
 
 export const RecipesContext = createContext();
 
 const RecipesContextProvider = (props) => {
-    const [recipes, setRecipes] = useState([]);
+    const [recipes, SetRecipes] = useState([]);
 
     useEffect(() => {
+        const loadRecipes = async () => {
+            const loadedRecipes = await databaseManager.getRecipes();
+            SetRecipes(loadedRecipes);
+        }
         loadRecipes();
     }, []);
 
-    const checkConnectivity = async () => {
-        const networkState = await Network.getNetworkStateAsync();
-        return networkState.isConnected && await checkServerAvailability();
-    };
-
-    const synchronizeRecipes = async () => {
-        if (await checkConnectivity()) {
-            await deleteAllRecipesFromServer();
-            const localRecipes = await databaseManager.getRecipes();
-            for (const recipe of localRecipes) {
-                await addRecipeToServer(recipe);
-            }
-            console.log("connected to server");
-        } else {
-            const loadedRecipes = await databaseManager.getRecipes();
-            setRecipes(loadedRecipes);
-            console.log("connected to localDb");
-        }
-    };
-
-    const loadRecipes = async () => {
-        await synchronizeRecipes();
-        setRecipes(await databaseManager.getRecipes());
-    };
-
     const addRecipe = async (newRecipe) => {
-        if (await checkConnectivity()) {
-            await addRecipeToServer(newRecipe);
-            await databaseManager.addRecipe(newRecipe);
-        } else {
-            await databaseManager.addRecipe(newRecipe);
-        }
-        await loadRecipes();
-    };
+        await databaseManager.addRecipe(newRecipe);
+        const updatedRecipes = await databaseManager.getRecipes();
+        SetRecipes(updatedRecipes);
+    }
 
     const updateRecipe = async (updatedRecipe) => {
-        if (await checkConnectivity()) {
-            await updateRecipeOnServer(updatedRecipe.id, updatedRecipe);
-            await databaseManager.updateRecipe(updatedRecipe);
-        } else {
-            await databaseManager.updateRecipe(updatedRecipe);
-        }
-        await loadRecipes();
-    };
+        await databaseManager.updateRecipe(updatedRecipe);
+        const updatedRecipes = await databaseManager.getRecipes();
+        SetRecipes(updatedRecipes);
+    }
 
     const deleteRecipe = async (id) => {
-        if (await checkConnectivity()) {
-            await deleteRecipeFromServer(id);
-            await databaseManager.deleteRecipe(id);
-        } else {
-            await databaseManager.deleteRecipe(id);
-        }
-        await loadRecipes();
-    };
+        await databaseManager.deleteRecipe(id);
+        const updatedRecipes = await databaseManager.getRecipes();
+        SetRecipes(updatedRecipes);
+    }
 
     return (
-        <RecipesContext.Provider value={{ recipes, addRecipe, updateRecipe, deleteRecipe }}>
+        <RecipesContext.Provider value={{recipes, addRecipe, updateRecipe, deleteRecipe}}> 
             {props.children}
         </RecipesContext.Provider>
     );
-};
-
+}
+ 
 export default RecipesContextProvider;
